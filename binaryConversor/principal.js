4552f7921df9c667db7272aa3ad4d2d99d20bc02
Matheus <mgguima95@gmail.com>
2021-03-06 23:49:11 -0300

    New repository

---
+++ binaryConversor/principal.js
@@ -0,0 +1,34 @@
+function executaConversao(evento) {
+    evento.preventDefault();
+    let numBinario = inputBin.value;
+    let somaTotal = 0;
+    let valor;
+    let c = 0;
+  //  numBinario = parseInt(numBinario, 2);
+    for(let i = numBinario.length - 1; i >= 0; i--) {
+        valor = numBinario[c] * Math.pow(2, i);
+        somaTotal += valor;
+        c++;
+    }
+    numBinario = somaTotal;
+    inputDec.value = numBinario;
+}
+
+const inputBin = document.querySelector('.binNumber');
+const inputDec = document.querySelector('.decNumber')
+const button = document.querySelector('.button');
+const mensagemErro = document.querySelector('.errorMessageApagado');
+button.addEventListener('click', executaConversao);
+inputBin.addEventListener('input', testaInput);
+
+function testaInput(evento) {
+    inputDec.value = '';
+    mensagemErro.classList.add('errorMessageApagado');
+    if(inputBin.value.length > 0) {
+        if(evento.data < 0 || evento.data >= 2 || !evento.data.match(/\d/)) {
+            mensagemErro.classList.remove('errorMessageApagado');
+            mensagemErro.classList.add('errorMessage');
+            inputBin.value = '';
+        }
+    }
+}
